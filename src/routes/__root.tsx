b66087b6f72ac307b696d23b5740a411f723f85a
Agência Progrex <agenciaprogrex@users.noreply.github.com>
2026-09-16 15:51:14 -0400

    Adicionar rastreamento Microsoft Clarity

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -125,6 +125,15 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="pt-BR">
       <head>
         <HeadContent />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window,document,"clarity","script","yjd1c8ah63");`,
+          }}
+        />
       </head>
       <body>
         {children}
